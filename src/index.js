468b81c604a7490fdec850b73fc7366655afb45b
Vishnu7086 <vishnumahawar1006@gmail.com>
2024-06-18 12:41:20 +0530

    Custom api response and error handling using ( Middlewares and Utilities )

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+gegsefsef
+sfs
