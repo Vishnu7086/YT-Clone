bb8168a479f5162b2c6cd6d133898bf182935944
Vishnu7086 <vishnumahawar1006@gmail.com>
2024-06-20 19:47:23 +0530

    Routes and Controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -17,6 +18,6 @@ connectDB()
         console.log(`Server is running at port : ${process.env.PORT}`)
     })
 })
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
+.catch((error) => {
+    console.log("MONGO db connection failed !!! ", error);
 })
